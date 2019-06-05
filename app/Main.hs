67bc6a9f87e9ae7aa20ae1302bcb57549d817650
CoderPuppy <coderpup@gmail.com>
2019-06-05 16:25:59 -0400

    Display any "existing" data for the output (if it came directly from an album, artist or playlist). Also display Spotify URIs for all the tracks

--- app/Main.hs
+++ app/Main.hs
@@ -28,11 +28,15 @@ main = do
 	putStrLn $ case tracks of
 		Ordered _ -> "ordered"
 		Unordered _ -> "unordered"
+	maybe (pure ()) T.putStrLn $ flip fmap (existing val) $ \case
+		ExArtist ar -> "artist " <> artistName ar <> " (spotify:artist:" <> artistId ar <> ")"
+		ExAlbum al -> "album " <> albumName al <> " featuring " <> formatList "no-one" (fmap artistName $ albumArtists al) <> " (spotify:album:" <> albumId al <> ")"
+		ExPlaylist pl -> "playlist " <> playlistName pl <> " (spotify:playlist:" <> playlistId pl <> ")"
 	for_ (tracksList tracks) $ \track -> T.putStrLn $ mconcat [
 			trackName track,
 			" by ", formatList "no-one" $ fmap artistName $ trackArtists track,
 			" from ", albumName $ trackAlbum track,
-			" (", trackId track, ")"
+			" (spotify:track:", trackId track, ")"
 		]
 
 formatList :: (IsString a, Semigroup a, Foldable f) => a -> f a -> a
